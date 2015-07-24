71df40caa4349ab960d86f2fec02dc18f40bbc7a
nabijaczleweli <nabijaczleweli@gmail.com>
2015-07-24 20:40:21 +0200

    Add branch to PR deploy folder names
    [ci skip]

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]
-	then export DROPBOX_SUBFOLDER_NAME=PR#$TRAVIS_PULL_REQUEST-`echo $TRAVIS_REPO_SLUG | sed s:/:_:g | tr [:upper:] [:lower:]`
+	then export DROPBOX_SUBFOLDER_NAME=PR#$TRAVIS_PULL_REQUEST-`echo $TRAVIS_REPO_SLUG | sed s:/:_:g | tr [:upper:] [:lower:]`-$TRAVIS_BRANCH
 	else export DROPBOX_SUBFOLDER_NAME=$TRAVIS_BUILD_NUMBER-$TRAVIS_COMMIT_RANGE
 fi
 
