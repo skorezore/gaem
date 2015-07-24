34c034ea098273a3eb02715a0e0f3dad32385ac4
nabijaczleweli <nabijaczleweli@gmail.com>
2015-07-24 21:31:55 +0200

    Add branch to stadnard build deploy folder names
    [ci skip]

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]
 	then export DROPBOX_SUBFOLDER_NAME=PR#$TRAVIS_PULL_REQUEST-`echo $TRAVIS_REPO_SLUG | sed s:/:_:g | tr [:upper:] [:lower:]`-$TRAVIS_BRANCH
-	else export DROPBOX_SUBFOLDER_NAME=$TRAVIS_BUILD_NUMBER-$TRAVIS_COMMIT_RANGE
+	else export DROPBOX_SUBFOLDER_NAME=$TRAVIS_BUILD_NUMBER-$TRAVIS_COMMIT_RANGE-$TRAVIS_BRANCH
 fi
 
 curl -Os "https://raw.githubusercontent.com/andreafabrizi/Dropbox-Uploader/master/dropbox_uploader.sh"
