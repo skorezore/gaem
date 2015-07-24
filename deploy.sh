f0409bdefe03513caa9c2ae9c5dd63164491540f
nabijaczleweli <nabijaczleweli@gmail.com>
2015-07-24 20:35:33 +0200

    Don't rename "binaries/gaem". Add fancy directory name to "deploy.sh"

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-curl -Oq "https://raw.githubusercontent.com/andreafabrizi/Dropbox-Uploader/master/dropbox_uploader.sh"
+if [ "$TRAVIS_PULL_REQUEST" != "false" ]
+	then export DROPBOX_SUBFOLDER_NAME=PR#$TRAVIS_PULL_REQUEST-`echo $TRAVIS_REPO_SLUG | sed s:/:_:g | tr [:upper:] [:lower:]`
+	else export DROPBOX_SUBFOLDER_NAME=$TRAVIS_BUILD_NUMBER-$TRAVIS_COMMIT_RANGE
+fi
+
+curl -Os "https://raw.githubusercontent.com/andreafabrizi/Dropbox-Uploader/master/dropbox_uploader.sh"
 chmod +x dropbox_uploader.sh
-./dropbox_uploader.sh -f .dropbox_uploader mkdir $TRAVIS_BUILD_NUMBER
-./dropbox_uploader.sh -f .dropbox_uploader upload binaries/gaem-$TRAVIS_BUILD_NUMBER-$COMPILER_NAME $TRAVIS_BUILD_NUMBER/gaem-$TRAVIS_BUILD_NUMBER-$COMPILER_NAME
+./dropbox_uploader.sh -f .dropbox_uploader mkdir $DROPBOX_SUBFOLDER_NAME
+./dropbox_uploader.sh -f .dropbox_uploader upload binaries/gaem $DROPBOX_SUBFOLDER_NAME/gaem-$TRAVIS_BUILD_NUMBER-$COMPILER_NAME
 
