ed388a2326a9acbf491523d01c25fa5cd2726880
nabijaczleweli <nabijaczleweli@gmail.com>
2015-07-28 11:27:29 +0200

    Don't upload assets into assets
    [ci skip]

--- deploy.sh
+++ deploy.sh
@@ -7,4 +7,4 @@ chmod +x dropbox_uploader.sh
 
 ./dropbox_uploader.sh -f .dropbox_uploader mkdir "$dropbox_subfolder_name"
 ./dropbox_uploader.sh -f .dropbox_uploader upload binaries/gaem "$dropbox_subfolder_name/gaem-$TRAVIS_BUILD_NUMBER-$COMPILER_NAME"
-./dropbox_uploader.sh -sf .dropbox_uploader upload assets "$dropbox_subfolder_name/assets"
+./dropbox_uploader.sh -sf .dropbox_uploader upload assets "$dropbox_subfolder_name"
