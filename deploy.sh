149c33b8ec452a65047cd315153419cde7332563
nabijaczleweli <nabijaczleweli@gmail.com>
2015-08-16 17:37:07 +0200

    Also upload cpponfig DLL

--- deploy.sh
+++ deploy.sh
@@ -8,3 +8,4 @@ chmod +x dropbox_uploader.sh
 ./dropbox_uploader.sh -f .dropbox_uploader mkdir "$dropbox_subfolder_name"
 ./dropbox_uploader.sh -f .dropbox_uploader upload binaries/gaem "$dropbox_subfolder_name/gaem-$TRAVIS_BUILD_NUMBER-$COMPILER_NAME"
 ./dropbox_uploader.sh -sf .dropbox_uploader upload assets "$dropbox_subfolder_name"
+./dropbox_uploader.sh -sf .dropbox_uploader upload binaries/*cpponfig.* "$dropbox_subfolder_name"
