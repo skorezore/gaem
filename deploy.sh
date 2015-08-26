f9d2185ac7574bfe8bead495037d5ac84d4a409e
nabijaczleweli-remote <nabijaczleweli@gmail.com>
2015-08-26 11:50:53 +0000

    Add line report deployment

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 dropbox_subfolder_name=$TRAVIS_BUILD_NUMBER-$TRAVIS_COMMIT_RANGE-$TRAVIS_BRANCH
 
@@ -6,6 +6,14 @@ curl -Os "https://raw.githubusercontent.com/andreafabrizi/Dropbox-Uploader/maste
 chmod +x dropbox_uploader.sh
 
 ./dropbox_uploader.sh -f .dropbox_uploader mkdir "$dropbox_subfolder_name"
-./dropbox_uploader.sh -f .dropbox_uploader upload binaries/gaem "$dropbox_subfolder_name/gaem-$TRAVIS_BUILD_NUMBER-$COMPILER_NAME"
-./dropbox_uploader.sh -sf .dropbox_uploader upload assets "$dropbox_subfolder_name"
-./dropbox_uploader.sh -sf .dropbox_uploader upload binaries/*cpponfig.* "$dropbox_subfolder_name"
+case "$1" in
+	"")
+		./dropbox_uploader.sh -f .dropbox_uploader upload binaries/gaem "$dropbox_subfolder_name/gaem-$TRAVIS_BUILD_NUMBER-$COMPILER_NAME"
+		./dropbox_uploader.sh -sf .dropbox_uploader upload assets "$dropbox_subfolder_name"
+		./dropbox_uploader.sh -sf .dropbox_uploader upload binaries/*cpponfig.* "$dropbox_subfolder_name"
+		;;
+	--report)
+		./dropbox_uploader.sh -sf .dropbox_uploader upload report.html "$dropbox_subfolder_name/report.html"
+		;;
+esac
+
