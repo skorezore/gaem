89b5621f343619e171a8487b33117a0c2e9ef6c2
nabijaczleweli-vm <nabijaczleweli@gmail.com>
2015-09-27 14:40:00 +0100

    Fix deployment

--- deploy.sh
+++ deploy.sh
@@ -1,29 +1,20 @@
 #!/bin/bash
 
-uploader="./dropbox_uploader.sh -f .dropbox_uploader"
-subfolder_name="$TRAVIS_BUILD_NUMBER-$TRAVIS_COMMIT_RANGE-$TRAVIS_BRANCH"
-temporary_folder=$(mktemp -d /tmp/gaem_deploy.XXXXXXXXXX)
+uploader="echo ./dropbox_uploader.sh -f .dropbox_uploader"
 
 upload_default=true
-explicit_upload_default=false
 
 while [[ $# -gt 0 ]]
 do
 	case $1 in
 		--report)
 			upload_report=true
-			upload_default=false
-			;;
-		--tarball)
-			upload_tarball=true
-			upload_default=false
 			;;
 		--newest)
 			upload_newest=true
-			upload_default=false
 			;;
-		--default)
-			explicit_upload_default=true
+		--no-default)
+			upload_default=""
 			;;
 		*)
 			echo "Unknown argument \"$1\"! Ignoring."
@@ -37,47 +28,20 @@ curl -Os "https://raw.githubusercontent.com/andreafabrizi/Dropbox-Uploader/maste
 chmod +x dropbox_uploader.sh
 
 
-[[ -n $upload_default ]] && upload_default=$explicit_upload_default
-[[ $upload_newest ]] && subfolder_name="$subfolder_name 0-newest"
+if [[ $upload_default ]]
+then
+	subfolder_name="$TRAVIS_BUILD_NUMBER-$TRAVIS_COMMIT_RANGE-$TRAVIS_BRANCH"
+	$uploader mkdir "$subfolder_name"
+	$uploader upload "build/gaem" "$subfolder_name/gaem-$TRAVIS_BUILD_NUMBER-$COMPILER_NAME"
+	$uploader upload "assets" "$subfolder_name/assets"
+	[[ $upload_report ]] && $uploader upload "report.html" "$subfoldr_name/report.html"
+fi
 
+if [[ $upload_newest ]]
+then
+	$uploader mkdir "0-newest"
+	$uploader upload "build/gaem" "0-newest/gaem-$COMPILER_NAME"
+	$uploader upload "assets" "0-newest/assets"
+	[[ $upload_report ]] && $uploader upload "report.html" "0-newest/report.html"
+fi
 
-for folder in $subfolder_name
-do
-	if [[ $folder = *newest* ]]
-	then
-		binarysuffix="$COMPILER_NAME"
-	else
-		binarysuffix="$TRAVIS_BUILD_NUMBER-$COMPILER_NAME"
-	fi
-
-	all_files_temporary_folder=$(mktemp --dry-run allfiles.XXXXXXXXXX)
-	mkdir "$temporary_folder/$all_files_temporary_folder"
-
-	cp "build/gaem" "$temporary_folder/$all_files_temporary_folder/gaem-$buildffix"
-	cp -r "assets" "$temporary_folder/$all_files_temporary_folder/"
-
-	[[ $upload_report ]] && cp "report.html" "$temporary_folder/$all_files_temporary_folder/"
-
-
-	for folder in $subfolder_name
-	do
-		mv "$temporary_folder/$all_files_temporary_folder" "$temporary_folder/$folder"
-
-		pushd "$temporary_folder" > /dev/null
-		tar -caf "$folder-$COMPILER_NAME.tar.bz2" "$folder"
-		popd > /dev/null
-
-		mv "$temporary_folder/$folder" "$temporary_folder/$all_files_temporary_folder"
-	done
-
-
-	$uploader mkdir "$folder"
-
-	[[ $upload_default ]] && $uploader upload "$temporary_folder/$all_files_temporary_folder"/* "$folder"
-	[[ $upload_report ]] && $uploader -s upload "$temporary_folder/$all_files_temporary_folder/report.html" "$folder"
-	[[ $upload_tarball ]] && $uploader upload "$temporary_folder/$folder-$COMPILER_NAME.tar.bz2" "$folder"
-done
-
-$uploader delete "0-newest/gaem-$TRAVIS_BUILD_NUMBER-$COMPILER_NAME" || true
-
-rm -rf "$temporary_folder"
